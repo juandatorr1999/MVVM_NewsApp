ccf169ab58902c13737d382d8d2b85c2613518bd
Juan David Torres <juandatorr_1999@hotmail.com>
2021-06-04 18:54:14 -0500

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Juan David Torres  on 04/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
